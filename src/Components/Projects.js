325bfbeaf9b2acb21e8f65c99985da53785029fd
KioiJr <mutungajoshua13@gmail.com>
2022-11-16 22:18:45 +0300

    semi final

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom'
 import hides from "../Hides&Skins.png"
-import hosp from "../Hospital.png"
-import Andromeda from "../Andromedamovies.png"
-import movie from './movie';
+import andromeda from "../Andromedamovies.png";
+import hosp from "../Hospital.png";
 
 
 const Projects = () => {
+
+    // const [goToMovie, setGoToMovie] = React.useState(false);
+    // const [goToHosp, setToHosp] = React.useState(false);
+    // const [goToHides, setToHides] = React.useState(false);
+
+    // if(goToMovie){
+    //     return <Navigate to = "./Movie" />
+    // }
+    // if (goToHosp){
+    //     return <Navigate to = "./Hospital" />
+    // }
+    // if (goToHides){
+    //     return <Navigate to = "./Hides" />
+    // }
+  
+    
   return (
     <div id="project" className="container mt-3 pb-5">
             <div className="post-heading text-center">
@@ -19,6 +35,9 @@ const Projects = () => {
                                 <h6 className="card-title">Hospital Management System</h6>
                             </div>
                             <img src= {hosp} className="card-img-top" alt=""/>
+                            
+                            {/* <button className='readMore'onClick={()=>{setToHosp(true)}}>Read more</button> */}
+                            
                         </div>
                     </div>
 
@@ -28,10 +47,12 @@ const Projects = () => {
                             <div className="card-body">
                                 <h6 className="card-title"> Movie Place</h6>
                             </div>
-                            <img src={Andromeda} className="card-img-top" alt=""/>
-                            <a className="" href="#movie">Read more...</a>
+                            <img src={andromeda} className="card-img-top" alt=""/>
+                            {/* <button onClick={() => {setGoToMovie(true); }}>
+                                Read More</button> */}
+			  			</div> 
                         </div>
-                    </div>
+                    
         
                     <div className="col-lg-4 col-md-6 col-12 pb-5">
                         <div className="card">
@@ -40,13 +61,13 @@ const Projects = () => {
                                 <h6 className="card-title">Hides and Skins Management System</h6>
                             </div>
                             <img src={hides} className="card-img-top" alt=""/>
+                            {/* <button onClick={()=>{setToHides(true)}}>Read more</button> */}
                         </div>
                     </div>
-
+                    </div>
                 </div>
             </div>
-        </div>
-
+        
   )
 }
 
